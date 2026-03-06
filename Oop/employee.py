bb9297ca0400d05fd101450edb879250bcb5d3db
Dev Gam <gamalielezekiel140@gmail.com>
2026-03-06 17:50:54 +0100

    working with oop files

--- Oop/employee.py
+++ Oop/employee.py
@@ -24,21 +24,22 @@ class Employee:
         return f"Employee name {self.name} \n Id : {self.id} \n Age : {self.age} \n Salary : {self.salary}"
     
 user = Employee( 2, "gam", 30 , 3002 )
-print(user)
-
+user1 = Employee( 5, "dapo", 3000 , 30020000 )
+print(user.emp_info())
+print(user1.emp_info())
 ### getting the data 
 ### you can use the dot notation to get the attribute of the class 
-print(user.name, user.age , user.salary , user.id)
-### note that data can be dynamic 
-### setting the value
+# print(user.name, user.age , user.salary , user.id)
+# ### note that data can be dynamic 
+# ### setting the value
 
-user.name = "Amina"
-user.id = 5
-user.salary = 20000.000
+# user.name = "Amina"
+# user.id = 5
+# user.salary = 20000.000
 
-print(user)
-engineer = Employee(4 , "mamuel", 38 , 332.02 )
-print(engineer)
-teacher = Employee(8 , "hilma", 40 , 30078.4 )
-print(teacher)
+# print(user)
+# engineer = Employee(4 , "mamuel", 38 , 332.02 )
+# print(engineer)
+# teacher = Employee(8 , "hilma", 40 , 30078.4 )
+# print(teacher)
 
